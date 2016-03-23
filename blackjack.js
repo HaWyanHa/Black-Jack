19ce80b65febe64ff39817414cdfc99456eb823f
footclan2k2 <HaWyanHa@Ryans-MacBook-Pro-2.local>
2016-03-23 16:44:10 -0400

    Almost done

---
+++ blackjack.js
@@ -0,0 +1,73 @@
+
+function runGame() {
+
+    var display = document.querySelector('figure');
+    var cards = ['A', '2','3', '4','5','6','7','8', '9', '10', 'J', 'Q', 'K'];
+
+    function hit() {
+        var card = Math.floor(Math.random() * cards.length);
+        display.innerHTML = display.innerHTML + ' ' + cards[card];
+        checkResult(false, true);
+    }
+
+    function checkResult(standing, hitting) {
+       var deal = display.innerHTML.split(' ');
+
+
+        var cardValue = 0;
+
+
+        deal.forEach(function(card, i) {
+            if (Number(card)) {
+                cardValue = cardValue + Number(card);
+            }
+
+            if (card === 'J' || card === 'Q' || card === 'K') {
+                cardValue = cardValue + 10;
+            }
+
+            if (card === 'A') 
+                cardValue = cardValue + 11; 
+        });
+
+        if (cardValue < 15 && standing) {
+            alert('Dealer wins.');
+            start();
+        }
+        else if (cardValue <= 18 && standing) {
+            alert('Push!');
+            start();
+        }
+        else if (cardValue > 21) {
+            alert('You Bust.');
+            start();
+        }
+
+        else if ((cardValue > 18 && hitting) || cardValue === 21) {
+            alert('You win!');
+            start();
+        }
+
+
+    }
+
+    document.getElementById('stand').addEventListener('click',function(){ 
+        checkResult(true, false); 
+    });
+    function start (){
+        function randomCard(){
+        card = Math.floor(Math.random() * cards.length);
+        display.innerHTML = cards[card];
+    }
+        card = Math.floor(Math.random() * cards.length);
+        display.innerHTML = display.innerHTML + ' ' + cards[card];
+
+    checkResult();
+}
+    start();
+    document.getElementById('hit').addEventListener('click', function(){
+        hit();
+    });
+}
+
+runGame();
